84e8e03e2b90afc7dd6221bf0c4dc62b520b2457
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-07-24 21:49:41 -0300

    feat: created recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(verbose_name='Nome', max_length=200)
+    ingredients = models.TextField(verbose_name='Ingredientes')
+    method_preparation = models.TextField(verbose_name='Modo de preparo')
+    cooking_time = models.IntegerField(verbose_name='Tempo de preparo')
+    recipe_yield  = models.CharField(verbose_name='Rendimento', max_length=100)
+    category  = models.CharField(verbose_name='Categoria', max_length=100)
+    created_at = models.DateTimeField(verbose_name='Criado em', default=datetime.now, blank=True)
