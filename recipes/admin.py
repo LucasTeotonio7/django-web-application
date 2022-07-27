b69ffa004909d335926ebe51639db5db0035df06
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-07-26 21:40:12 -0300

    feat: customizing django admin for recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Recipe
 
-admin.site.register(Recipe)
+class RecipeList(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'cooking_time')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'category')
+    list_filter = ('category', )
+    list_per_page = 10
+
+
+admin.site.register(Recipe, RecipeList)
