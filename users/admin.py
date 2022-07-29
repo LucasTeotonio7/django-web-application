162fc350827c3237a5e2f071d611fa14c3340f76
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-07-28 21:13:21 -0300

    feat: registered user model in admin' ;

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User
+# Register your models here
 
-# Register your models here.
+
+class UserList(admin.ModelAdmin):
+    list_display = ('id', 'username', 'full_name', 'email', 'is_active')
+    list_display_links = ('id', 'username')
+    search_fields = ('name', 'email')
+    list_filter = ('is_active', )
+    list_per_page = 10
+
+
+admin.site.register(User, UserList)
