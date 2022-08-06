fd583d2af16f4fa0bae94dba9e887cae78581f83
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-06 12:50:11 -0300

    feat: registered users route

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('users/registration/', views.users_registration, name='users-registration'),
-    path('users/login/', views.users_login, name='users-login'),
-    path('users/logout/', views.users_logout, name='users-logout'),
+    path('registration/', views.users_registration, name='users-registration'),
+    path('login/', views.users_login, name='users-login'),
+    path('logout/', views.users_logout, name='users-logout'),
     path('dashboard/', views.dashboard, name='users-dashboard')
 ]
