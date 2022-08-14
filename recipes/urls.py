5420c85ee5bb1053ed83c49d125127890c29edce
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-14 11:03:13 -0300

    feat: created view to delete recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('recipes/<int:pk>', views.recipe, name='recipes')
+    path('recipes/<int:pk>', views.recipe_detail, name='recipe_detail'),
+    path('recipes/<int:pk>/delete', views.recipe_delete, name='recipe_delete')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Recipe
 
 
 def index(request):
-        
+
     recipes = Recipe.objects.filter(published=True).order_by('-created_at')
     if 'search' in request.GET:
         recipes = recipes.filter(name__icontains=request.GET['search'])
@@ -12,7 +12,12 @@ def index(request):
     return render(request, 'index.html', context=data)
 
 
-def recipe(request, pk):
+def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     data = {'recipe' : recipe}
     return render(request, 'recipes.html', context=data)
+
+def recipe_delete(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    recipe.delete()
+    return redirect('users_dashboard')
