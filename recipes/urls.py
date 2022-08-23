86f3cafdd7483d301c68c46039a40fd5b9e6b8b7
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-08-22 22:15:41 -0300

    refactor: refactored recipe views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/recipe/', views.recipe_create, name='recipe_create'),
-    path('recipes/<int:pk>', views.recipe_detail, name='recipe_detail'),
-    path('recipes/<int:pk>/delete', views.recipe_delete, name='recipe_delete'),
-    path('recipes/<int:pk>/update', views.recipe_update, name='recipe_update')
+    path('', index, name='index'),
+    path('create/recipe/', recipe_create, name='recipe_create'),
+    path('recipes/<int:pk>', recipe_detail, name='recipe_detail'),
+    path('recipes/<int:pk>/delete', recipe_delete, name='recipe_delete'),
+    path('recipes/<int:pk>/update', recipe_update, name='recipe_update')
 ]
--- recipes/views/recipes.py
+++ recipes/views/recipes.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Recipe
+from recipes.models import Recipe
 
 
 def index(request):
